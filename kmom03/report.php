2656eebd349c490ad89a5c621912e674bb31e947
ogdocat <ogdocat@gmail.com>
2013-09-13 22:56:29 +0200

    Copy kmom02 folder and rename it kmom3.

---
+++ kmom03/report.php
@@ -0,0 +1,59 @@
+<?php 
+include("incl/config.php");
+$title = "Redovisningsrapport";
+$pageId = "report";
+$pageStyle = null;
+?>
+
+<?php include 'incl/header.php'; ?>
+
+<div id="content">
+  <h1>Redovisningsrapport</h1>
+  <h3>kmom01</h3>
+  <hr>
+  <p>
+  	Detta första kursmoment har inte presenterat några svårigheter för mig att genomföra.<br>
+  	Gillar upplägget att göra en redovisning på detta sätt, 
+  	och tänker att vad vi gör etsar sig fast lite bättre i minnet, då man tvingas reflektera över vad vi kodat i denna rapport.</p>
+  <p>
+  	Som operativsystem och dator använder jag en mac laptop med OS 10.8.<br>
+  Mac är bra för mig eftersom det är ett stabilt unix system jag även kan ha photoshop på.</p>
+  <p>
+  	Jag har oftast Chrome och Firefox öppna samtidigt, och använder ibland Opera.</p>
+  <p>
+  	Normalt sett använder jag unix command line för sftp, ssh och slutligen vim för texthantering.<br>
+  	Om det behövs mycket klipp och klistra i texten så använder jag programmet sublimetext. </p>
+  <p>
+  	Min lokala utvecklingsmiljö är en hemmagjord AMP stack.<br>
+  	Just nu: Apache 2.2.22 som kom med OSX, mysql 5.6.13 och php 5.5.3</p>
+  <hr>
+
+  <h3>kmom02</h3>
+  <hr>
+  <p>
+    Kursmoment nr.2 gick bra att genomföra, då jag har erfarenhet av CSS. Enkla och tydligt förklarande uppgifter.<br>
+    Likaså tycker jag om guiderna html20 och css20. Enkla och bra för repetition. Har även tittat på php20.
+  <p>
+
+    Jag fastnade lite i tankar om hur jag bäst kan indentera och kommentera min CSS-kod för framtiden.<br>
+    Prövade också några olika sätt att gruppera CSS-koden, och kom fram till ett för mig lättläst resultat.<br> 
+    Ta gärna en titt på:
+    <a href="http://www.student.bth.se/~nibc13/htmlphp/kmom02/viewsource.php?dir=style&file=stylesheet.css" title="http://www.student.bth.se/~nibc13/htmlphp/kmom02/viewsource.php?dir=style&file=stylesheet.css">stylesheet.css</a>
+    för att även ge mig feedback på min hacker-signatur.
+  <p>
+    Jag förvandlade samtliga divs till html5 element, lade till html5shiv och valde att alltid visa scroll bar.<br>
+    Firebug är ett ovärderligt redskap. Jag använder också Chromes developer tools, som jag rekommenderar.<br>
+    Strukturen med kataloger och incl är bra och jag ser fram emot att senare börja utveckla objekt orienterat.<br>
+    Som CSS3 element valde jag några rundade hörn och skuggor. Skapade även en gräslig <a href="http://www.student.bth.se/~nibc13/htmlphp/kmom02/me2.php" title="http://local.dev/~macbook/htmlphp/kmom02/me2.php">me2.php</a> variant.
+  <p>  
+    <a href="http://www.student.bth.se/~nibc13/htmlphp/kmom02/me.php" title="http://www.student.bth.se/~nibc13/htmlphp/kmom02/me.php">http://www.student.bth.se/~nibc13/htmlphp/kmom02/me.php</a>
+
+
+
+
+
+  
+
+</div>
+
+<?php include 'incl/footer.php'; ?>
---
+++ kmom03/viewsource.php
@@ -0,0 +1,22 @@
+<?php 
+include("incl/config.php");
+$title = "Källkoden";
+$pageTitle = "Visa källkod";
+$pageId = "source";
+
+// Include code from source.php to display sourcecode-viewer
+$sourceBasedir=dirname(__FILE__);
+$sourceNoEcho=true;
+include("incl/source.php");
+$pageStyle=$sourceStyle;
+?>
+
+<?php include("incl/header.php"); ?>
+
+<div id="content">
+<?php echo "$sourceBody"; ?>
+<hr>
+</div>
+
+<?php include("incl/footer.php"); ?>
+
